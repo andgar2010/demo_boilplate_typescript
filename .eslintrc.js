7a54d615311b5a95471a1ebe0a141290d00299ce
Andrés García Dev <dev@tech-andgar.me>
2020-03-15 06:23:17 +0100

    Fix eslint basic typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,41 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  'root': true,
+  'parser': '@typescript-eslint/parser',
   parserOptions: {
     sourceType: 'module',
     project: './tsconfig.json',
   },
-  extends:  [
-    'airbnb',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    'prettier',
+    // 'airbnb',
+    // 'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    // 'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
-  settings: {
-    react: {
-      'version': '999.999.999'
-    }
-  },
+  plugins: [
+    '@typescript-eslint',
+    'prettier'
+  ],
   rules: {
-    'import/extensions': [
-       'error',
-       'ignorePackages',
-       {
-         'js': 'never',
-         'jsx': 'never',
-         'ts': 'never',
-         'tsx': 'never'
-       }
-    ]
- }
-
+    'prettier/prettier': 2 // Means error
+  },
+//   settings: {
+//     react: {
+//       'version': '999.999.999'
+//     }
+//   },
+//   rules: {
+//     'import/extensions': [
+//        'error',
+//        'ignorePackages',
+//        {
+//          'js': 'never',
+//          'jsx': 'never',
+//          'ts': 'never',
+//          'tsx': 'never'
+//        }
+//     ]
+//  }
 }
